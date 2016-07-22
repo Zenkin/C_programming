87b90f8e8f2b18511e55e2d1b4941293766f5ebb
Anton Lamtev <antonlamtev@gmail.com>
2016-07-22 17:15:28 +0300

    Update input_data.c

--- translation_seconds/App/input_data.c
+++ translation_seconds/App/input_data.c
@@ -2,11 +2,15 @@
 
 const long int max_input_seconds = 31536000;
 
+//считать данные - очень обще. по сути ты считываешь только seconds
 long int input_data()
 {
+    //существует правило: объявляй переменные как можно ближе к месту их использования
+    //согласно нему, их можно перенести ниже
     long int input_seconds;
+    
+    //пессимистичный подход. сначала данные неверные )
     bool invalid_values = true;
-
     puts("Enter seconds(not more 31'536'000):");
     printf("Input seconds: ");
     while (invalid_values)
@@ -15,11 +19,16 @@ long int input_data()
         if ((input_seconds > max_input_seconds) || (input_seconds < 0))
         {
             print_error();
+            //лишняя строчка.
             invalid_values = true;
         }
         else
         {
-            invalid_values = false;
+            invalid_values = false; //при таком подходе, возможно будет более эффективно
+            //избежать еще одной проверки invalid_values.
+            //выполнив здесь break;
+            //но и так тоже хорошо.
+            //хотя.. вообще кажется, что можно обойтись без break и без лишних сравнений
         }
     }
     return input_seconds;
